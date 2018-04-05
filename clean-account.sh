d905123e82974461ee16bdef86ed09853b89ec07
System Administrator <root@emily12.berkeley.edu>
2018-04-05 13:29:01 -0700

    Set cleanup to work only for one user.

--- clean-account.sh
+++ clean-account.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-HOMEDIR="$HOME"
-
-for i in Desktop Downloads Documents
-    do echo "Deleting $HOMEDIR/${i} contents";
-    rm -rf $HOMEDIR/${i}/*
+#If we ever need to deleted a logged-in console user,
+#then uncomment the last line two lines and replace the current
+#"for i" loop.
+#for i in `who | grep "console" | cut -d" " -f1`
+#    do HOMEDIR="$(eval echo ~${i})";
+for i in neyman 
+    do HOMEDIR="/Users/neyman";
+    echo "Cleaning $HOMEDIR";
+    for i in Desktop Downloads Documents
+        do echo "Deleting $HOMEDIR/${i} contents";
+        rm -rf $HOMEDIR/${i}/*
+    done
 done
+
