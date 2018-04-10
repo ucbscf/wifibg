e9106546819cd9044b9fcc03c64e56bac615d4b9
Luis Torres <lrtorres@elrond.berkeley.edu>
2018-04-10 16:48:10 -0700

    Fix always killing the dock on login.

--- update-snp-background.sh
+++ update-snp-background.sh
@@ -10,10 +10,10 @@ if [ ! -f "/Library/Desktop Pictures/${IMAGE}_Anno.jpg" ] ; then
 fi
 
 if [ -f "/Users/${USER}/wifibg.log" ] ; then
-    
-    if [ "$(cmp --silent /Users/${USER}/wifibg.log) /var/log/wifibg/wifibg.log)" == "1" ] ; then 
+    COMP=$(cmp --silent /Users/${USER}/wifibg.log /var/log/wifibg/wifibg.log ; echo $?)
+    if [ "$COMP" == "1" ] ; then
         killall Dock
-    	cp /var/log/wifibg/wifibg.log /Users/${USER}/wifibg.log
+        cp /var/log/wifibg/wifibg.log /Users/${USER}/wifibg.log
     fi
 else
     cp /var/log/wifibg/wifibg.log /Users/${USER}/wifibg.log
