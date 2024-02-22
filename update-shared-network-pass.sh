aaa3a6c2371e496c6e0a9fa852a88999fe14ae18
ryanlovett <rylo@berkeley.edu>
2024-02-22 10:22:36 -0800

    Use updated backdrop and text style.

--- update-shared-network-pass.sh
+++ update-shared-network-pass.sh
@@ -11,14 +11,14 @@ fi
 PASS=$(/usr/bin/openssl rand -base64 6)
 HOST=$(hostname -s)
 USER=$(id -n -u)
-IMAGE="Sierra"
+IMAGE="Zoom backdrops-spring-sunrise"
 
 echo "USER: ${USER} ${HOST} Airport Password: ${PASS}" 
 echo "USER: ${USER} ${HOST} Airport Password: ${PASS}" >> /var/log/wifibg/wifibg-manual-run.log
 
 security add-generic-password -a AirPort -l en1 -j "Internet-sharing password" -s com.apple.network.wlan.swap.ssid.en1 -w "${PASS}" -U
 
-$CONVERT "/Library/Desktop Pictures/$IMAGE.jpg" -font Arial -weight 100 -pointsize 75 -gravity North -draw "fill black text 15,120 \"${HOST} Airport Password: ${PASS}\"" "/Library/Desktop Pictures/$IMAGE"_Anno.jpg
+$CONVERT "/Library/Desktop Pictures/$IMAGE.jpg" -font Arial -weight 80 -pointsize 36 -gravity North -draw "fill black text 520,60 \"Neyman WiFi Password: ${PASS}\"" "/Library/Desktop Pictures/$IMAGE"_Anno.jpg
 
 networksetup -setairportpower en1 off
 sleep 2
